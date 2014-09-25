1f46b21679638da2c6bf3d2bd4602a00d3e66605
Jack Liu <jack980517@gmail.com>
2014-09-25 22:20:59 +0800

    Refine push.py

--- push.py
+++ push.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 # push.py: Formats the tab-seperated csv file, and push it to the phone via adb.
 import os
+songname='mandora'
 f=open('hard.csv')
 a=f.read().split('\n')
 f.close()
@@ -9,4 +10,4 @@ for i in range(0,len(a)):
 	while a[i].endswith('\t'):a[i]=a[i][:-1]
 	f.write('%s\n'%a[i])
 f.close()
-os.system('adb push ./hard.txt /sdcard/Android/data/com.playstation.psstore/files/psm/npqa00103/application/assets/songs/mandora/mandora.hard.txt')
+os.system('adb push ./hard.txt /sdcard/Android/data/com.playstation.psstore/files/psm/npqa00103/application/assets/songs/%s/%s.hard.txt'%(songname,songname))
